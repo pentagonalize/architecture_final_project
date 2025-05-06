cdc7d7ee0548095ddbf60e41a043db8c4f34c84f
Cristian Villatoro <108214320+cristianvill@users.noreply.github.com>
2025-05-06 13:57:30 -0400

    Update python_script_to_exe.sh
    Explanation comments

--- exe_creation_files/python_script_to_exe.sh
+++ exe_creation_files/python_script_to_exe.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Requires have python environment with pytorch and pyinstaller 
+# pip install torch
+# pip install -pyinstaller
+
 echo "Creating python scripts"
 sh mlp_create_loop_pytorch.sh
 echo "Converting python scripts into exe files"
